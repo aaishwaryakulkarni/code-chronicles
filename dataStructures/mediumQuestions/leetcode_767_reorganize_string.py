5f99f526ad43dff1acbb1923c80fe396b3ea8485
aaishwaryakulkarni <aishwarya.kulkarni09@gmail.com>
2024-06-14 15:42:19 +0100

    leetcode sums

---
+++ dataStructures/mediumQuestions/leetcode_767_reorganize_string.py
@@ -0,0 +1,22 @@
+"""
+767. Reorganize String
+
+Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+Return any possible rearrangement of s or return "" if not possible.
+
+ 
+Example 1:
+Input: s = "aab"
+Output: "aba"
+
+Example 2:
+Input: s = "aaab"
+Output: ""
+
+"""
+
+def reorganizeString(s):
+    pass
+
+s = "aaabb"
+print(reorganizeString(s))
--- dataStructures/topInterviewProblems/leetcode_200_number_of_islands.py
+++ dataStructures/topInterviewProblems/leetcode_200_number_of_islands.py
@@ -31,49 +31,45 @@ Output: 3
 import collections
 
 def numIslands(grid):
-
+    
+	if not grid:
+		return 0
+	
+	rows, cols = len(grid), len(grid[0])
 	visited = set()
 	islands = 0
-
-	rows = len(grid)
-	cols = len(grid[0])
-
-	directions = [[-1,0], [1,0],[0,-1],[0,1]]
+	directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 	def bfs(r, c):
-
 		q = collections.deque()
+
 		visited.add((r, c))
 		q.append((r, c))
 
 		while q:
+			r, c = q.popleft()
 
-			row, col = q.popleft()
+			for dr, dc in directions:
 
-			for dr in directions:
+				new_dr = r + dr
+				new_dc = c + dc
 
-				new_r, new_c = row + dr[0], col + dr[1]
+				if (new_dr in range(rows) and 
+					new_dc in range(cols) and
+					grid[new_dr][new_dc] == "1" and
+					(new_dr, new_dc) not in visited):
 
-				if ((new_r in range(rows)) and 
-					(new_c in range(cols)) and
-					(grid[new_r][new_c] == '1') and
-					(new_r, new_c) not in visited):
-
-					q.append((new_r, new_c))
-					visited.add((new_r, new_c))
-
+					q.append((new_dr, new_dc))
+					visited.add((new_dr, new_dc))
 
 	for r in range(rows):
 		for c in range(cols):
-
-			if((grid[r][c] == '1') and 
-				((r,c) not in visited)):
-
+			if grid[r][c] == "1" and (r, c) not in visited:
 				bfs(r, c)
-
-				islands = islands + 1
-
+				islands += 1
+	
 	return islands
+
 
 grid = [
   ["1","1","0","0","0"],
